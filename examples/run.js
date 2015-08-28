2bd1eb481964baf1ba77c658f1518506a3d43d15
Mikhail Kuzin <mikhail@textmagic.biz>
2015-08-28 14:32:31 +0700

    add npm package.json file

--- examples/run.js
+++ examples/run.js
@@ -1,5 +1,5 @@
 var TMClient = require('../index.js')
 var path = './' + process.argv[2] + '.js';
-var c = new TMClient('priidik', 'C7XDKZOQZo6HvhJPtU0MBcWlfqwtp8');
+var c = new TMClient(process.argv[3], process.argv[4]);
 
 require(path)(c);
